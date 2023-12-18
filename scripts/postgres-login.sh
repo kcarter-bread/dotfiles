6f9548c3a03575e7a68a7c80017413c272e78015
kcarter-bread <kenneth.carter@breadfinancial.com>
2023-12-18 07:52:51 -0800

    testing

--- scripts/postgres-login.sh
+++ scripts/postgres-login.sh
@@ -4,6 +4,9 @@ echo "which account"
 
 read account 
 
+echo "writer endpoint is: "
+AWS_PROFILE=bd-gbl-${account}-admin aws rds describe-db-clusters --db-cluster-identifier bd-ue2-${account}-postgres | jq '.DBClusters | .[] | .Endpoint'
+
 echo "username is: " 
 AWS_PROFILE=bd-gbl-${account}-admin aws ssm get-parameters --names "/aurora_postgres/primary_aurora_postgres_master_username" --with-decryption | jq '.Parameters | .[] | .Value'
 
